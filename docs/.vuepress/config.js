0c6731219afef274831d425be5761d0f89f2614d
jermbo <jermbo@cosmicstrawberry.com>
2018-11-16 15:23:37 -0500

    starting documentation pages

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: "Cosmic Typer",
+  description: "Typer thing for others to do",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/guide/" },
+      { text: "About", link: "https://github.com/jermbo" }
+    ]
+  }
+};
