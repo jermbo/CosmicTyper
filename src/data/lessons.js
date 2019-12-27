7c7a4fd3a80832e748576c3b83078280beecbe26
jermbo <jermbo@cosmicstrawberry.com>
2019-12-27 15:02:24 -0500

    adding more lessons

--- src/data/lessons.js
+++ src/data/lessons.js
@@ -27,27 +27,27 @@ export default [
           "This is the H1 heading tag. Our most important tag. Type out the tag and the words in between.",
         action: ["<h1>hello</h1>"],
         render: true
+      },
+      {
+        type: "dom",
+        desc:
+          "This is the H1 heading tag. Our most important tag. Type out the tag and the words in between.",
+        action: ["<h2>", "  world", "</h2>"],
+        render: true
+      },
+      {
+        type: "style",
+        desc: "Now let's all paragraph tags",
+        action: ["h1 {", "  font-size: 12px;", "  color: #444;", "}"],
+        render: true
+      },
+      {
+        type: "dom",
+        desc:
+          "This is the H1 heading tag. Our most important tag. Type out the tag and the words in between.",
+        action: ["<span>hello world</span>"],
+        render: true
       }
-      // {
-      //   type: "dom",
-      //   desc:
-      //     "This is the H1 heading tag. Our most important tag. Type out the tag and the words in between.",
-      //   action: ["<h2>", "  world", "</h2>"],
-      //   render: true
-      // },
-      // {
-      //   type: "style",
-      //   desc: "Now let's all paragraph tags",
-      //   action: ["h1 {", "  font-size: 12px;", "  color: #444;", "}"],
-      //   render: true
-      // },
-      // {
-      //   type: "dom",
-      //   desc:
-      //     "This is the H1 heading tag. Our most important tag. Type out the tag and the words in between.",
-      //   action: ["<span>hello world</span>"],
-      //   render: true
-      // }
     ]
   },
   {
