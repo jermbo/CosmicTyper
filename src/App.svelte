d66ed24da35cad852870e0a98f783b2a2d588b04
Jermbo <jermbo@cosmicstrawberry.com>
2020-02-04 15:46:35 -0500

    adding back app state and apply grid

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,37 @@
 <script>
+  // Life Cycle
+  import { beforeUpdate } from "svelte";
+
+  // Components
+  import AppState from "UI/AppState.svelte";
   import Nav from "Global/Nav.svelte";
+  import WelcomeScreen from "./WelcomeScreen.svelte";
+
+  // Stores
+  import { APP_STATE, IS_DEBUG } from "Stores/AppState.js";
+  import { USER_OBJ } from "Stores/UserState.js";
+
+  // Helpers and Enums
+  import { setLsItem } from "Scripts/LocalStorageHelper.js";
+  import { LSKeyEnums, AppStateEnums } from "Scripts/enum.js";
+
+  // Reactive variables
+  $: appState = $APP_STATE.state;
+
+  const rightNow = Date.now();
+  // Setting Threshold to 18 hours.
+  const resetThreshold = 1000 * 60 * 60 * 18;
+
+  function changeAppState({ detail }) {
+    APP_STATE.setState(detail);
+  }
+
+  beforeUpdate(() => {
+    if (rightNow - $USER_OBJ.active_time > resetThreshold) {
+      localStorage.clear();
+      window.location.reload();
+    }
+  });
 </script>
 
 <style lang="scss" global>
@@ -7,19 +39,13 @@
 </style>
 
 <main>
+  {#if $IS_DEBUG}
+    <AppState />
+  {/if}
   <Nav />
 
-  <!-- <div class="welcome">
-    <header>
-      <h1>
-        <small>Welcome to</small>
-        Typer
-      </h1>
-      <p>
-        This is a tool to help you type better. With a little practice every
-        day, you will get faster and more accurate.
-      </p>
-    </header>
-  </div> -->
+  {#if !appState || appState == AppStateEnums.appStart}
+    <WelcomeScreen on:updateState={changeAppState} />
+  {/if}
 
 </main>
