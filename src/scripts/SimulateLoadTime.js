d63af64976dc5e9e80b6efe7cbe68aab022ce638
Jermbo <jermbo@cosmicstrawberry.com>
2020-03-20 13:18:17 -0400

    Starting Fresh

--- src/scripts/SimulateLoadTime.js
+++
@@ -1,7 +0,0 @@
-export default function SimulateLoadTime(data, waitTime = 250) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(data);
-    }, waitTime);
-  });
-}
--- src/stores/CodeState.js
+++
@@ -1,4 +0,0 @@
-import { writable } from "svelte/store";
-
-export const HTML_CODE = writable([]);
-export const CSS_CODE = writable([]);
