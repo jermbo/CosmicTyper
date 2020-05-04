e9fa4cf91f5ef8f4d998da2ca1174f77d8aa076d
Jermbo <jermbo@cosmicstrawberry.com>
2020-05-04 13:48:37 -0400

    👌 IMPROVE: adding foundation for auth

---
+++ src/pages/admin/_layout.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { goto } from "@sveltech/routify";
+  // import { authorize } from "my-auth-service";
+  function authorize() {
+    return new Promise((resolve, reject) => {
+      if (Math.random() > 0.3) {
+        resolve("You are logged in");
+      } else {
+        reject("Need to test reject");
+      }
+    });
+  }
+</script>
+
+{#await authorize()}
+  Checking permissions...
+{:then}
+  <slot />
+{:catch}
+  {$goto('/')}
+{/await}
