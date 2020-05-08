2e5dfc8983c55e8251c690c7683f9ced2c2bcb98
Jermbo <jermbo@cosmicstrawberry.com>
2020-05-08 07:52:51 -0400

    📦 NEW: Starting foundation for viewing / editing

---
+++ src/pages/admin/web-lessons/[lessonId]/edit.svelte
@@ -0,0 +1,6 @@
+<script>
+  import { context } from "@sveltech/routify";
+  console.log($context); /** /blog/:postId/view **/
+  import { params } from "@sveltech/routify";
+  console.log($params); /** {userId: 123, postId: 456} **/
+</script>
---
+++ src/pages/admin/web-lessons/index.svelte
@@ -0,0 +1,40 @@
+<script>
+  import LessonsList from "../../../components/LessonsList.svelte";
+  // let webData = [];
+  let display = false;
+
+  const getData = (async () => {
+    const resp = await fetch("http://localhost:1337/web-lessons");
+    return await resp.json();
+  })();
+</script>
+
+<style>
+  pre {
+    height: 50vh;
+    overflow-y: auto;
+  }
+</style>
+
+<!-- routify:options name="admin-web-lessons" -->
+<div class="container">
+  <p>Web Lessons Admin</p>
+  <!-- <pre>{JSON.stringify(webData, null, 2)}</pre>
+  {#await getData}
+    <p>Getting data</p>
+  {:then lessons}
+    <pre>{JSON.stringify(lessons, null, 2)}</pre>
+    <!--  -->
+  <!-- {:catch error}
+    <p>something went wrong {error}</p>
+  {/await} -->
+
+  {#await getData}
+    <p>...waiting</p>
+  {:then lessons}
+    <pre>{JSON.stringify(lessons, null, 2)}</pre>
+    <!-- <LessonsList baseURL="admin/web-lessons" {lessons} /> -->
+  {:catch error}
+    <p>An error occurred!</p>
+  {/await}
+</div>
