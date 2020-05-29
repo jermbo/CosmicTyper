5f6eff88109cc1e523cac3eaf10f257f6305e883
Jermbo <jermbo@cosmicstrawberry.com>
2020-05-28 21:31:54 -0400

    👌 IMPROVE: adding form validation

--- src/store/web-data.js
+++ src/store/web-data.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-import { setWebLessons } from "./store";
 import { API_URL, parseList } from "../utils";
+import { state } from "./store";
 
-export async function getWebLessonsAction() {
+const getWebLessonsAction = async () => {
   try {
     const resp = await axios.get(`${API_URL}/web-lessons`);
     const data = parseList(resp);
@@ -11,4 +11,30 @@ export async function getWebLessonsAction() {
   } catch (err) {
     return console.log(err);
   }
-}
+};
+
+const setWebLessons = (lessons) => {
+  state.webLessons.update((old) => lessons);
+};
+
+const updateWebLesson = (id) => {
+  const lesson = getLsItem(KEY_ENUMS.webKey).filter((lesson) => lesson.id == id)[0];
+  // TODO - Refactor when creating user session
+  // lesson.hasCompleted = !lesson.hasCompleted;
+  state.webLessons.update((old) => {
+    const index = old.findIndex((l) => l.id == lesson.id);
+    old.splice(index, 1, lesson);
+    setLsItem(KEY_ENUMS.webKey, old);
+    return [...old];
+  });
+};
+
+const addWebLesson = (lesson) => {
+  state.webLessons.update((old) => [lesson, ...old]);
+};
+
+const deleteWebLesson = (id) => {
+  state.webLessons.update((old) => old.filter((l) => l.id !== id));
+};
+
+export { getWebLessonsAction, setWebLessons, updateWebLesson, addWebLesson, deleteWebLesson };
