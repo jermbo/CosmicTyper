7ee7d40022091c5815c5964237e9c3bd0a165967
Jermbo <jermbo@cosmicstrawberry.com>
2020-06-10 15:42:29 -0400

    👌 IMPROVE: converted welcome

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,15 +1,11 @@
 <!-- routify:options name="home" -->
-<section class="hero is-fullheight-with-navbar">
-  <div class="hero-body">
-    <div class="container has-text-centered ">
-      <h1 class="title has-text-dark">
-        <span class="has-text-weight-light">Welcome to Code Palm Beach</span>
-        <br />
-        <span class="has-text-weight-bold">Student Typer</span>
-      </h1>
-      <p class="subtitle has-text-dark has-text-weight-bold has-margin-top-7">
-        A tool to help you learn to type, while learning to code HTML and CSS.
-      </p>
-    </div>
-  </div>
+<section class="welcome">
+  <h1 class="title">
+    <span class="title__thin">Welcome to Code Palm Beach</span>
+    <br />
+    <span class="title__heavy">Student Typer</span>
+  </h1>
+  <p class="subtitle">
+    A tool to help you learn to type, while learning to code HTML and CSS.
+  </p>
 </section>
