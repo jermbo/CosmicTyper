f9d24913459bbe9898c3bb38e509209314814972
Jermbo <jermbo@cosmicstrawberry.com>
2020-06-28 21:28:52 -0400

    👌 IMPROVE: cleaned up welcome dom and styles

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,11 +1,11 @@
 <!-- routify:options name="home" -->
 <section class="welcome">
-  <h1 class="title">
-    <span class="title__thin">Welcome to Code Palm Beach</span>
+  <h1 class="welcome__title">
+    <span class="-thin">Welcome to Code Palm Beach</span>
     <br />
-    <span class="title__heavy">Student Typer</span>
+    <span class="-heavy">Student Typer</span>
   </h1>
-  <p class="subtitle">
+  <p class="welcome__subtitle">
     A tool to help you learn to type, while learning to code HTML and CSS.
   </p>
 </section>
