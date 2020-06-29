81dd2a6d31dda3f0dd6f39448ded78f0245c09ff
Jermbo <jermbo@cosmicstrawberry.com>
2020-06-28 22:44:07 -0400

    👌 IMPROVE: updating typing admin area

--- src/components/common-ui/CodeBlock.svelte
+++ src/components/common-ui/CodeBlock.svelte
@@ -2,4 +2,5 @@
   export let data = {};
 </script>
 
+<label class="label">Raw Data</label>
 <pre>{JSON.stringify(data, null, 2)}</pre>
--- src/pages/admin/typing-lessons/[lessonId].svelte
+++ src/pages/admin/typing-lessons/[lessonId].svelte
@@ -36,13 +36,9 @@
   async function updateLesson() {
     try {
       const id = editingLesson.id;
-      const resp = await axios.put(
-        `http://localhost:1337/typing-lessons/${id}`,
-        editingLesson,
-        {
-          headers: { Authorization: `Bearer ${$adminUser.token}` },
-        },
-      );
+      const resp = await axios.put(`${API_URL}/${id}`, editingLesson, {
+        headers: { Authorization: `Bearer ${$adminUser.token}` },
+      });
       const data = await resp.data;
       $goto("typing-lessons-admin");
     } catch (err) {
@@ -53,12 +49,9 @@
   async function deleteLesson() {
     try {
       const id = editingLesson.id;
-      const resp = await axios.delete(
-        `http://localhost:1337/typing-lessons/${id}`,
-        {
-          headers: { Authorization: `Bearer ${$adminUser.token}` },
-        },
-      );
+      const resp = await axios.delete(`${API_URL}/${id}`, {
+        headers: { Authorization: `Bearer ${$adminUser.token}` },
+      });
       const data = await resp.data;
       deleteTypingLesson(id);
       $goto("typing-lessons-admin");
@@ -70,7 +63,7 @@
   async function addLesson() {
     try {
       const resp = await axios.post(
-        `${API_URL}typing-lessons/`,
+        `${API_URL}/typing-lessons/`,
         editingLesson,
         {
           headers: { Authorization: `Bearer ${$adminUser.token}` },
@@ -101,82 +94,77 @@
 
 <!-- routify:options name="typing-lesson-single-admin" -->
 {#if editingLesson}
-  <div class="container">
-    <section class="page-container">
-      <header class="page">
-        <a class="button is-small is-info" href={$url('typing-lessons-admin')}>
-          Back
-        </a>
-        <h1 class="page__title">{editingLesson.title || 'The Lesson'}</h1>
+  <div class="page-header">
+    <div class="page-header__info">
+      <h1 class="page__title">{editingLesson.title || 'The Lesson'}</h1>
+    </div>
+    <div class="page-actions">
+      <div class="page__actions">
+        {#if editingLesson.id}
+          <button
+            class="button is-small is-primary"
+            on:click|preventDefault={updateLesson}
+            disabled={!isValid}>
+            Update lesson
+          </button>
 
-        <div class="page__actions">
-          {#if editingLesson.id}
-            <button
-              class="button is-small is-primary"
-              on:click|preventDefault={updateLesson}
-              disabled={!isValid}>
-              Update lesson
-            </button>
-
-            <button
-              class="button is-small is-danger is-outlined"
-              href={null}
-              on:click|preventDefault={deleteLesson}>
-              Delete lesson
-            </button>
-          {:else}
-            <button
-              class="button is-small is-primary"
-              href={null}
-              on:click|preventDefault={addLesson}
-              disabled={!isValid}>
-              Add Lesson
-            </button>
-          {/if}
-        </div>
-      </header>
-
-      <div class="page-body" />
-    </section>
+          <button
+            class="button is-small is-danger is-outlined"
+            href={null}
+            on:click|preventDefault={deleteLesson}>
+            Delete lesson
+          </button>
+        {:else}
+          <button
+            class="button is-small is-primary"
+            href={null}
+            on:click|preventDefault={addLesson}
+            disabled={!isValid}>
+            Add Lesson
+          </button>
+        {/if}
+      </div>
+    </div>
   </div>
-  <div class="columns">
-    <div class="column is-half">
-      <div class="field">
-        <label class="label">Lesson Title</label>
-        <div class="control">
+
+  <div class="page-body">
+    <div class="admin-lesson">
+      <div class="lesson-edit">
+
+        <div class="form-field">
+          <label class="label">Lesson Title</label>
           <input
             class="input"
             class:is-danger={errors.title}
             type="text"
             bind:value={editingLesson.title}
             placeholder="Lesson Title" />
+          {#if errors.title}
+            <p class="help is-danger">{errors.title}</p>
+          {/if}
         </div>
-        {#if errors.title}
-          <p class="help is-danger">{errors.title}</p>
-        {/if}
-      </div>
 
-      <div class="field">
-        <label class="label">Difficulty</label>
-        <div class="control">
-          <div class="select">
-            <select bind:value={editingLesson.difficulty}>
-              {#each DIFFICULTY_TYPES as type}
-                <option value={type}>{type}</option>
-              {/each}
-            </select>
-          </div>
+        <div class="form-field">
+          <label class="label">Difficulty</label>
+          <select class="select" bind:value={editingLesson.difficulty}>
+            {#each DIFFICULTY_TYPES as type}
+              <option value={type}>{type}</option>
+            {/each}
+          </select>
         </div>
-      </div>
 
-      <AdminTypingSteps
-        bind:steps={editingLesson.steps}
-        stepError={errors.steps}
-        emptyLines={errors.emptyLines} />
-    </div>
-    <div class="column is-half">
-      <p>Raw Data</p>
-      <CodeBlock data={editingLesson} />
+        <AdminTypingSteps
+          bind:steps={editingLesson.steps}
+          stepError={errors.steps}
+          emptyLines={errors.emptyLines} />
+
+      </div>
+      <!-- /lesson-edit -->
+
+      <div class="lesosn-raw">
+        <CodeBlock data={editingLesson} />
+      </div>
+      <!-- /lesson-raw-->
     </div>
   </div>
 {/if}
