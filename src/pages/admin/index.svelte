93c947f8cdee93552029b1ac0743471a36942968
Jermbo <jermbo@cosmicstrawberry.com>
2020-07-01 16:24:42 -0400

    👌 IMPROVE: ensure all pages have proper structure

--- src/pages/admin/index.svelte
+++ src/pages/admin/index.svelte
@@ -3,19 +3,13 @@
 </script>
 
 <!-- routify:options name="admin" -->
-<section class="page">
-  <header class="page-header">
-    <div class="page-header__info">
-      <h1 class="page__title">Typer Admin Page</h1>
-      <p class="page__subtitle">
-        Please select an area you would like to view.
-      </p>
-    </div>
-  </header>
-  <div class="page-body">
-    <a class="button" href={$url('web-lessons-admin')}>Web Lessons Admin</a>
-    <a class="button" href={$url('typing-lessons-admin')}>
-      Typing Lessons Admin
-    </a>
+<header class="page-header">
+  <div class="page-header__info">
+    <h1 class="page__title">Typer Admin Page</h1>
+    <p class="page__subtitle">Please select an area you would like to view.</p>
   </div>
-</section>
+</header>
+<div class="page-body">
+  <a class="button" href={$url('web-lessons-admin')}>Web Lessons Admin</a>
+  <a class="button" href={$url('typing-lessons-admin')}>Typing Lessons Admin</a>
+</div>
