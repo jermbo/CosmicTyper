a9cd0227a4caee41b1e0c239658bef31ffc48988
jermbo <jermbo@users.noreply.github.com>
2026-06-09 12:59:03 -0400

    🗑️ REMOVE: Clean up project structure by deleting unused configuration files, assets, and components, transitioning to a more streamlined setup with Vite and SvelteKit.

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import Nav from "./Nav.svelte";
-import Redirect from "./Redirect.svelte";
-import LessonsList from "./LessonsList.svelte";
-import WebLesson from "./WebLesson.svelte";
-import TypeLesson from "./TypeLesson.svelte";
-
-export { Nav, Redirect, LessonsList, WebLesson, TypeLesson };
--- src/store/enums.js
+++
@@ -1,4 +0,0 @@
-export const keyEnums = {
-  webKey: "web-lessons",
-  typingKey: "typing-lessons",
-};
